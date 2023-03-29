c70a13df3e64fedfe189e9293c552b5f6000b575
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2023-03-28 23:18:32 -0300

    findById and Update Department Implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -12,6 +12,16 @@ public class Program2 {
 		Department newDepartment = new Department(null, "Home appliances");
 		depDao.insert(newDepartment);
 		System.out.println("Inserted! New Id = " + newDepartment.getId());
+		
+		System.out.println("\n=== TEST 2: Department findById =====");
+		newDepartment = depDao.findById(3);
+		System.out.println(newDepartment);
+		
+		System.out.println("\n=== TEST 3: Department update =====");
+		newDepartment = depDao.findById(1);
+		newDepartment.setName("Cars");
+		depDao.update(newDepartment);
+		System.out.println("Update Completed! ");
 	}
 
 }
